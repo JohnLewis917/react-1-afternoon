83eca01c8d2cb2cf7b46dccedb75304e75173483
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-12 12:25:38 -0600

    done

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -3,7 +3,7 @@ import EvenAndOdd from '../Topics/EvenAndOdd';
 import FilterObject from '../Topics/FilterObject';
 import FilterString from '../Topics/FilterString';
 import Palindrome from '../Topics/Palindrome';
-import Sum from '../Topics/Sum'
+import Sum from '../Topics/Sum';
 
 export default class TopicBrowser extends Component {
     render () {
